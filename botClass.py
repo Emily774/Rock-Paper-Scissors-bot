ba56f4e8fec1b2f940c2592319d9c52c463f9379
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-09-24 09:58:23 +0100

    unsure

--- botClass.py
+++ botClass.py
@@ -1,2 +1,38 @@
 
 abstract class bot:
+
+__init__
+
+variables = ['waterballon', 'rock', 'paper', 'scissors', 'dynamite']
+#OR could make classes where each bot calls a funtion to print its associated item?
+output = random(0, varaibles.length - 1)
+'self.' variables remain?
+
+export output
+
+
+In seperate file:
+    import output from bot1 as bot1Output
+    import output from bot2 as bot2Output
+
+if bot1Output == 'dynamite' && dynamite counter => 100:
+    bot1()
+    else if bot2Output == 'dynamite' && dynamite counter => 100:
+    bot2()
+
+    if output1 == output2:
+    print('It's a draw!)
+    if bot1Output == 'rock' && bot2Output == 'scissors':
+    bot1.counter ++
+
+#etc
+if output == dynamite:
+
+dynamite counter ++
+
+ At the end have:
+    bot1()
+    bot2()
+    roundCounter++
+    if roundCounter == 1000:
+    print('Here endth the game')
